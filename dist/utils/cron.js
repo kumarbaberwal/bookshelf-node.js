b73aa9176722408ce45bb4e417e9cf595ae364de
kumarbaberwal <niteshkumarghn1@gmail.com>
2025-03-20 23:01:46 +0530

    cron -> CronJob

--- dist/utils/cron.js
+++ dist/utils/cron.js
@@ -4,10 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.cronJob = void 0;
-const cron_1 = __importDefault(require("cron"));
+const cron_1 = require("cron");
 const https_1 = __importDefault(require("https"));
 const config_1 = require("../configs/config");
-exports.cronJob = new cron_1.default.CronJob('*/14 * * * *', function () {
+exports.cronJob = new cron_1.CronJob('*/14 * * * *', function () {
     https_1.default.get(config_1.ENV_VARS.API_URL, (res) => {
         if (res.statusCode === 200) {
             console.log("GET request sent successfully");
